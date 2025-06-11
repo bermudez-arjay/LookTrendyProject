b161c67cd1a35dba6e8c1684eaf907d2d6b97646
José Miguel Herrera <joseherrera1988@hotmail.com>
2025-06-10 20:56:45 -0600

    configurando CI CD

--- myproject/resources/views/layouts/app.blade.php
+++ myproject/resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
-    <script src="{{ mix('/js/app.js') }}"></script>
+    @vite('resources/js/app.js')
     <style>
         @keyframes fadeIn {
             from { opacity: 0; transform: translateY(10px); }
